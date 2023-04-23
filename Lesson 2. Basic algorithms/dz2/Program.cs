7c3bdcfe198fa546b93a03c5a666f194bfe856d2
Ирина <>
2023-04-23 14:22:51 +0300

    gitignore

--- Lesson 2. Basic algorithms/dz2/Program.cs
+++ Lesson 2. Basic algorithms/dz2/Program.cs
@@ -1,10 +1,14 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-int num = ;
 
-if (num < 100 || num > 999) {
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 100) {
     Console.WriteLine("Третьей цифры нет");
 }
 else {
-    int thirdDigit = (num / 10) % 10;
+    int thirdDigit = (number / 10) % 10;
     Console.WriteLine("Третья цифра числа: " + thirdDigit);
 }
