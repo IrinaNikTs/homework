57542213052dcb0ff54882654f8c0836d7ab4532
Ирина <>
2023-04-23 14:53:24 +0300

    дни недели

---
+++ Lesson 2. Basic algorithms/dz3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 1) {
+    Console.WriteLine("Это понедельник");
+}
+else if(number == 2) 
+{
+    Console.WriteLine("Это вторник");
+}
+else if(number == 3) 
+{
+    Console.WriteLine("Это среда");
+}
+else if(number == 4) 
+{
+    Console.WriteLine("Это четверг");
+}
+else if(number == 5) 
+{
+    Console.WriteLine("Это пятница");
+}
+else if(number == 6) 
+{
+    Console.WriteLine("Суббота - это выходной");
+}
+else if(number == 7) 
+{
+    Console.WriteLine("Воскресенье - это выходной");
+}
+else {
+    Console.WriteLine("Не является днем недели");
+}
