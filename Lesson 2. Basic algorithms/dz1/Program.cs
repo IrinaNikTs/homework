b2316e7ea63ad4f6bd75cc1aff76b713f6d493c1
Ирина <>
2023-04-23 14:11:54 +0300

    корректировки5

--- Lesson 2. Basic algorithms/dz1/Program.cs
+++ Lesson 2. Basic algorithms/dz1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int a = 788;
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-int secondDigit = (a / 10) % 10;
+int secondDigit = (number / 10) % 10;
 Console.WriteLine("Вторая цифра числа: " + secondDigit);
