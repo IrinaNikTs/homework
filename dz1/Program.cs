841483e33fc698c9020a687e3421c6ae8a6eab43
Ирина <>
2023-04-23 12:28:14 +0300

    script

---
+++ dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = 5;
+int b = 7;
+
+if(a>b) max = a;
+if(a<b) max = b;
+if(a>b) min = b;
+if(a<b) min = a;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
