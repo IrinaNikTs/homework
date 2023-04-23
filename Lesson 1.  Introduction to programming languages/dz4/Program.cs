79b8e9e5b902dc63d62f8521f82b3764c71d7c9e
Ирина <>
2023-04-23 13:45:17 +0300

    корректировки

--- Lesson 1.  Introduction to programming languages/dz4/Program.cs
+++ Lesson 1.  Introduction to programming languages/dz4/Program.cs
@@ -1,8 +1,10 @@
 ﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-int N = 10;
+Console.WriteLine("Введите число: ");
 
-for (int a = 1; a <= N; a++) {
-    if (a % 2 == 0) {
-        Console.WriteLine(a);
-    }
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -N;
+
+while(i <= N) {
+    Console.Write(i+" ");
+    i++;
 }
