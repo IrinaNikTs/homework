31860e9988df6e5bb7a99304b27811a23b7328f4
Ирина <>
2023-04-29 13:28:52 +0300

    dz3

---
+++ Lesson3. Basic algorithms. Continuation/DZ3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = Math.Abs(ReadInt("Введите целое положительное число "));
+
+for (int i=1; i <= num; i++)
+{
+    System.Console.WriteLine($" {Math.Pow(i, 3)}");
+}
