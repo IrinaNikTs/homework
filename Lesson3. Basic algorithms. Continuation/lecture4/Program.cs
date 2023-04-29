4703d2b6965b90d9baf2fcc3551c32d28f078b05
Ирина <>
2023-04-29 12:36:44 +0300

    Lesson 4

--- Lesson3. Basic algorithms. Continuation/lecture4/Program.cs
+++ Lesson3. Basic algorithms. Continuation/lecture4/Program.cs
@@ -2,9 +2,17 @@
 // от 1 до N.
 
 // 5 -> 1, 4, 9, 16, 25.
-// 2 -> 1,4 int ReadInt(string text)
-// {
-//     System.Console.Write(text);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+// 2 -> 1,4 
 
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = ReadInt("Введите целое положительное число");
+
+for (int i=1; i <= num; i++)
+{
+    System.Console.WriteLine($" {Math.Pow(i, 2)}");
+}
